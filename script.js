ca6d9410839d925f3167be3226efb85e01ebc089
Seray1 <107807594+Seray1@users.noreply.github.com>
2022-10-22 13:14:30 +0100

    non responsive version of the website

---
+++ script.js
@@ -0,0 +1,31 @@
+// let stickyHeader = document.getElementById("stickyHeader")
+// let stickyHeaderTwo = document.getElementById("stickyHeader2")
+
+
+// window.onscroll = function(){ headerChange()}
+
+
+// function headerChange(){
+//     if(document.body.scrollTop === 0 || document.documentElement.scrollTop === 0){
+//         stickyHeader.style.display = "flex"
+//     }
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+//         console.log("less than 20")
+//         stickyHeader.style.backgroundColor = "#283C61"
+//       }  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+//         console.log("greater than 600")
+//         stickyHeader.style.display = "none"
+//         stickyHeaderTwo.style.display = "flex"
+//        }  if(document.body.scrollTop < 500 || document.documentElement.scrollTop < 500){
+//         console.log("less thann 500")
+//         stickyHeader.style.display = "flex"
+//         stickyHeaderTwo.style.display = "none"
+
+
+//       } else {
+
+//             stickyHeader.style.display = "flex"
+//             stickyHeaderTwo.style.display = "none"
+//       }
+
+// }
